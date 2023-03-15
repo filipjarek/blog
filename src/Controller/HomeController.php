c3fcf64e1778f6854c2fd01286e8ecef3c5c33aa
filipjarek <filipjarek24@gmail.com>
2023-03-15 13:26:30 +0100

    Add KnpPaginatorBundle

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request, PostRepository $postRepository, PaginatorInterface $paginator): Response
     {
+        $posts = $paginator->paginate(
+            $postRepository->findAll(),
+            $request->query->getInt('page', 1)
+        );
+        
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'posts' => $posts,
         ]);
     }
 }
