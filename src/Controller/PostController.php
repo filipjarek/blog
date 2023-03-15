a9c30ed202f38a3585a666f9b470f4248a5d8efe
filipjarek <filipjarek24@gmail.com>
2023-03-15 14:22:00 +0100

    Add PostController

---
+++ src/Controller/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PostRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class PostController extends AbstractController
+{
+    #[Route('/post/{slug}', name: 'app_post')]
+    public function index(string $slug, PostRepository $postRepository): Response
+    {
+        $post = $postRepository->findOneBySlug($slug);
+        
+        return $this->render('post/index.html.twig', [
+            'categorySlug' => $post->getCategory()->getSlug(),
+            'post' => $post,
+        ]);
+    }
+}
--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -39,28 +39,18 @@ class PostRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Post[] Returns an array of Post objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findOneBySlug(string $slug): ?Post
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.slug = :slug')
+            ->setParameter('slug', $slug)
+            ->leftJoin('p.category', 'c')
+            ->addSelect('c')
+            ->andwhere('p.isActive = true')
+            ->andWhere('c.isActive = true')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 
-//    public function findOneBySomeField($value): ?Post
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
